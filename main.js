ce783d00937bc761b754370036ce676a7b8622c6
M <48407846+mvahaste@users.noreply.github.com>
2023-03-22 21:51:31 +0200

    Added width toggle, custom font setting

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 // vars
 var notepad = document.getElementById("notepad");
 
+var options = document.getElementById("options");
+
+var expandButton = document.getElementById("expand");
 var themeButton = document.getElementById("theme");
+var fontButton = document.getElementById("font");
+var widthButton = document.getElementById("width");
 var fsUpButton = document.getElementById("fsup");
 var fsDownButton = document.getElementById("fsdown");
+
+var overlay = document.getElementById("overlay");
+
+var oCancelButton = document.getElementById("cancel");
+var oSaveButton = document.getElementById("save");
+
+var oFontInput = document.getElementById("font-input");
 
 var moon = document.getElementById("moon");
 var sun = document.getElementById("sun");
@@ -47,6 +59,18 @@ function setLight() {
 	}
 }
 
+function toggleWidth() {
+	if (notepad.classList.contains("thinner")) {
+		notepad.classList.remove("thinner");
+
+		localStorage.setItem("width", "normal");
+	} else {
+		notepad.classList.add("thinner");
+
+		localStorage.setItem("width", "thinner");
+	}
+}
+
 function fsUp() {
 	var fs = getComputedStyle(root).getPropertyValue("--font-size");
 	root.style.setProperty("--font-size", parseInt(fs) + 2 + "px");
@@ -59,6 +83,38 @@ function fsDown() {
 	root.style.setProperty("--font-size", parseInt(fs) - 2 + "px");
 
 	localStorage.setItem("font-size", parseInt(fs) - 2 + "px");
+}
+
+function toggleOptions() {
+	if (options.classList.contains("expanded")) {
+		options.classList.remove("expanded");
+		expandButton.classList.remove("flipped");
+	} else {
+		options.classList.add("expanded");
+		expandButton.classList.add("flipped");
+	}
+}
+
+function showOverlay() {
+	overlay.classList.remove("hidden");
+}
+
+function hideOverlay() {
+	overlay.classList.add("hidden");
+}
+
+function saveCustomFont() {
+	var font = oFontInput.value;
+
+	if (font) {
+		root.style.setProperty("--font", font);
+		localStorage.setItem("font", font);
+	} else {
+		root.style.setProperty("--font", "JetBrains Mono");
+		localStorage.removeItem("font");
+	}
+
+	hideOverlay();
 }
 
 function save() {
@@ -74,10 +130,20 @@ function load() {
 function init() {
 	notepad.addEventListener("input", save);
 
+	expandButton.addEventListener("click", toggleOptions);
 	themeButton.addEventListener("click", toggleTheme);
-
+	fontButton.addEventListener("click", showOverlay);
+	widthButton.addEventListener("click", toggleWidth);
 	fsUpButton.addEventListener("click", fsUp);
 	fsDownButton.addEventListener("click", fsDown);
+
+	oCancelButton.addEventListener("click", hideOverlay);
+	oSaveButton.addEventListener("click", saveCustomFont);
+
+	if (localStorage.getItem("font")) {
+		root.style.setProperty("--font", localStorage.getItem("font"));
+		oFontInput.value = localStorage.getItem("font");
+	}
 
 	if (localStorage.getItem("theme") === "dark") {
 		setDark();
@@ -89,6 +155,10 @@ function init() {
 
 	if (localStorage.getItem("font-size")) {
 		root.style.setProperty("--font-size", localStorage.getItem("font-size"));
+	}
+
+	if (localStorage.getItem("width") === "thinner") {
+		notepad.classList.add("thinner");
 	}
 
 	load();
